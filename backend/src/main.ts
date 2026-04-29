aa2b77a73a81125aa4dc9b9b8dd8e6217b57af7e
Moiz Hanif <moizhanif.dev@gmail.com>
2026-04-30 00:56:53 +0500

    Update CORS configuration to reflect origin dynamically and allow all methods/credentials for production

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: '*' });
+  app.enableCors({ origin: true, methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', credentials: true });
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3001);
 }
